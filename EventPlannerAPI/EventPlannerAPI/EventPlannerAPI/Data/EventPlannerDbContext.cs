cc2b492f4e3a2ecabf65099b1f682c9fb76677ba
jacomer2 <69131100+jacomer2@users.noreply.github.com>
2023-05-16 11:44:08 -0400

    - API updates
    - sql database functioning
    - TODO: Controller

--- EventPlannerAPI/EventPlannerAPI/EventPlannerAPI/Data/EventPlannerDbContext.cs
+++ EventPlannerAPI/EventPlannerAPI/EventPlannerAPI/Data/EventPlannerDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace EventPlannerAPI.Data
+﻿using EventPlannerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlannerAPI.Data
 {
-    public class EventPlannerDbContext
+    public class EventPlannerDbContext : DbContext
     {
+        public EventPlannerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
     }
 }
---
+++ EventPlannerAPI/EventPlannerAPI/EventPlannerAPI/Migrations/20230516153510_Initial Migration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPlannerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCode = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
