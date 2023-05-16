4dde2a69ce5af9c7720dbecdf9cea59dcfccda6b
jacomer2 <69131100+jacomer2@users.noreply.github.com>
2023-05-16 10:35:50 -0400

    API Setup
    - set up some aspects of API

---
+++ EventPlannerAPI/EventPlannerAPI/EventPlannerAPI/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace EventPlannerAPI.Models
+{
+    public class Event
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public long ZipCode { get; set; }
+
+        
+    }
+}
